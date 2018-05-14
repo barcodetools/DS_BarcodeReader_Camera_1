e178f20359846d734ef5e4fe0b00cf4188517910
Mike <info@barcodetools.com>
2018-05-14 10:05:12 +0300

    DataSymbol Barcode Reader Camera decoder Example 1
    How to decode barcode barcodes from Web camera using DataSymbol Barcode decoding SDK.
    More info on:
    http://www.DataSymbol.com

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axBarcodeDecoder1.AboutBox();
+        }
+
+        void OnBarcodeIn(object sender, AxBarcodeReaderLib._IReaderEvents_BarcodeInEvent e)
+        {
+            textBox1.Text = e.barcode;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //axBarcodeDecoder1.SetProperty("VideoDelayAfterDec", 300);
+            //axBarcodeDecoder1.SetProperty("VideoStretch", 1);
+            //axBarcodeDecoder1.SetProperty("VideoBeep", 0);
+            //axBarcodeDecoder1.VideoStart(0, 170, 120);
+
+            AxBarcodeReaderLib._IReaderEvents_BarcodeInEventHandler barcodeIn = new AxBarcodeReaderLib._IReaderEvents_BarcodeInEventHandler(OnBarcodeIn);
+            axBarcodeDecoder1.BarcodeIn += barcodeIn;
+
+            axBarcodeDecoder1.SetProperty("LinearVerify", true);
+            axBarcodeDecoder1.ShowImage = true;
+            //axBarcodeDecoder1.BarcodeTypes |= (int)BarcodeReaderLib.EBarcodeTypes.DataMatrix;
+            axBarcodeDecoder1.VideoStart(0, 0, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axBarcodeDecoder1.VideoStop();
+        }
+
+    }
+}
